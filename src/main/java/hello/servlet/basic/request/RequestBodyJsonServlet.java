855b5a7bfc0b94b99b06310c4e0adfce4e9a00ef
ttangzzi <cvbg0802@gmail.com>
2025-09-12 18:11:35 +0900

    Http 응답 데이터 API JSON

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -16,7 +16,7 @@ import java.nio.charset.StandardCharsets;
 @WebServlet(name="requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
 
-    private ObjectMapper obejctMapper = new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,7 +24,7 @@ public class RequestBodyJsonServlet extends HttpServlet {
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         System.out.println("messageBody = " + messageBody);
 
-        HelloData helloData = obejctMapper.readValue(messageBody, HelloData.class);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
         System.out.println("helloData.username = " + helloData.getUsername());
         System.out.println("helloData.age = " + helloData.getAge());
 
